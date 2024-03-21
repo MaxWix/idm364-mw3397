c0e0fd8df834765df462a74904643cad68e7b857
MaxWix <mw3397@drexel.edu>
2024-03-21 18:31:13 -0400

    Final

---
+++ .svelte-kit/generated/client/app.js
@@ -0,0 +1,32 @@
+export { matchers } from './matchers.js';
+
+export const nodes = [
+	() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5'),
+	() => import('./nodes/6'),
+	() => import('./nodes/7'),
+	() => import('./nodes/8')
+];
+
+export const server_loads = [];
+
+export const dictionary = {
+		"/": [~3],
+		"/cart": [4],
+		"/ceramics": [~5],
+		"/collection": [~6],
+		"/collection/[slug]": [~7,[2]],
+		"/plants": [~8]
+	};
+
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+
+	reroute: (() => {})
+};
+
+export { default as root } from '../root.svelte';
